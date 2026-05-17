1602e299e35dd43bb53b97677038722de11e29d0
Dominik Hladík <dominik.hladik@seznam.cz>
2026-05-17 18:48:32 +0200

    feat(app): sizing added

--- projects/design/src/app/app.config.ts
+++ projects/design/src/app/app.config.ts
@@ -1,8 +1,27 @@
+import { VERSION_INFO } from '@/generated/version-info';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withInMemoryScrolling,
+  withViewTransitions,
+} from '@angular/router';
+import { provideAppVersion } from 'ngx-app-version';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withViewTransitions(),
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+      }),
+    ),
+    provideAppVersion({
+      version: VERSION_INFO.version,
+    }),
+  ],
 };
--- projects/design/src/app/app.ts
+++ projects/design/src/app/app.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgxAppVersionDirective } from 'ngx-app-version';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,10 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'flex flex-col h-full',
+  },
+  hostDirectives: [NgxAppVersionDirective],
 })
 export class App {
   protected readonly title = signal('design');
